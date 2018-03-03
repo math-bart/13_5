acf0f5ae62242f71044bc079a05c1afafea50e2a
Bartosz Wojtaś <el.klides@op.pl>
2018-03-03 12:01:06 +0100

    A

---
+++ modules/OSinfo.js
@@ -0,0 +1,24 @@
+var os = require('os');
+var colors = require('colors');
+
+function getOSinfo() {
+  var type = os.type();
+  if(type === 'Darwin') {
+    type = 'OSX';
+  } else if(type === 'Windows_NT') {
+    type = 'Windows';
+  }
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var userInfo = os.userInfo();
+  console.log('hello'.green);
+  console.log('System:'.gray, type);
+  console.log('Release:'.red, release);
+  console.log('CPU model:'.cyan, cpu);
+  console.log('User name:'.yellow, userInfo.username);
+  console.log('Home dir:', userInfo.homedir);
+  var OStime = require('./OStime');
+  OStime.print();
+}
+
+exports.print = getOSinfo;
---
+++ modules/OStime.js
@@ -0,0 +1,12 @@
+var os = require('os');
+var colors = require('colors');
+
+function getOStime() {
+  var uptime = os.uptime();
+  var hours =  parseInt(uptime / 3600);
+  var minutes =  parseInt((uptime - hours * 3600) / 60);
+  var seconds =  (uptime - hours * 3600 - minutes * 60).toFixed(0);
+  console.log('Uptime:'.green, hours, 'h', minutes, 'min', seconds, 'sec');
+}
+
+exports.print = getOStime;
